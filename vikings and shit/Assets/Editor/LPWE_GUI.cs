6e6035f2072a5771f3ec86e703014113e44b7d52
MichalKsiazk <37849515+MichalKsiazk@users.noreply.github.com>
2018-09-10 14:27:00 +0200

    LPWE
    >design pattern update

---
+++ vikings and shit/Assets/Editor/LPWE_GUI.cs
@@ -0,0 +1,269 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LPWE))]
+public class LPWE_GUI : Editor {
+
+	int page_index = 0;
+	string[] page_names = new string[] {"Map Creator", "Mesh Editor", "World Painter", "Object Brush"};
+	GameObject lpwe_components;
+	Vector3 start_hit_point;
+	bool pressed = false;
+	DialogInfo sensitive_buttons = new DialogInfo();
+	int i = 0;
+	int st = 0;
+	bool search_on_visual = false;
+
+	public override void OnInspectorGUI()
+	{
+		LPWE main = (LPWE)target;
+		page_index = GUILayout.Toolbar(page_index, page_names);
+		SelectPage(main);
+		base.OnInspectorGUI();
+	} 
+
+
+	void OnSceneGUI() 
+	{
+
+		bool first_frame = false;
+		LPWE main = (LPWE)target;
+		Vector3 hit_point = new Vector3 ();
+
+		i++; st++;
+
+
+		search_on_visual = page_index == 2;
+		bool highlight = !search_on_visual || (search_on_visual && st == 4);
+
+		if(st == 10)
+		{
+			st = 0; 
+		}
+
+		if (main.chunks == null) 
+		{
+			main.terrain_creator.LoadChunkMap ();
+		}
+
+		List<SelectedVertices> selected_vertices = new List<SelectedVertices>();
+		Event guiEvent = Event.current;
+
+		HandleMouseInput(guiEvent, ref main, ref first_frame);
+		hit_point = NewHitPoint(ref main);
+
+		//Debug.Log(hit_point);
+		if(highlight)
+		{
+			selected_vertices = main.selectors.FindInRange (hit_point, main.selectors.radius, main.hit_point, search_on_visual);
+		}
+		main.highlights.vp = search_on_visual;
+		if(highlight)
+		{
+			main.highlights.Draw(selected_vertices);
+		}
+
+
+		Action(first_frame, guiEvent, ref main, hit_point, ref selected_vertices);
+
+		SceneView.RepaintAll();
+		HandleUtility.AddDefaultControl (GUIUtility.GetControlID (FocusType.Passive));
+	}
+
+	void HandleMouseInput(Event guiEvent, ref LPWE main, ref bool first_frame)
+	{
+		if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0) 
+		{
+			start_hit_point = main.selectors.ColliderRaycastDetection ();
+			pressed = true;
+			first_frame = true;
+		}
+		if (guiEvent.type == EventType.MouseUp && guiEvent.button == 0) 
+		{
+			pressed = false;
+		}
+	}
+
+	Vector3 NewHitPoint(ref LPWE main)
+	{
+		switch (main.hit_point) 
+		{
+			case HitPoint.HitPoint3D:
+				return main.selectors.ColliderRaycastDetection ();
+			case HitPoint.HitPoint2D:
+				return main.selectors.ColliderRaycastDetection ();
+			case HitPoint.SeaLevel2D: 
+				return main.selectors.AxisRaycastDetection();
+		}
+		return new Vector3(0,0,0);
+	}
+
+	void Action(bool first_frame, Event guiEvent, ref LPWE main, Vector3 hit_point, ref List<SelectedVertices> selected_vertices)
+	{
+		if (pressed && guiEvent.button == 0) 
+		{
+			/*  MeshEditor   - 1
+			 *  WorldPainter - 2
+			 *	ObjectBrush  - 3
+			 */ 
+
+
+			if((first_frame && main.event_type == DragType.OnClick) || (main.event_type == DragType.Drag && i % main.modulo == 0))
+			{	
+				i = 0;			
+				switch(page_index)
+				{
+					case 1:
+						MeshEditorAction(ref main, ref selected_vertices, hit_point);
+					 	break;
+					case 2:
+						PainterAction(ref main, ref selected_vertices, hit_point);
+						break;
+				}
+			}
+		}
+	}
+
+	void MeshEditorAction(ref LPWE main, ref List<SelectedVertices> selected_vertices, Vector3 hit_point)
+	{
+		switch(main.mesh_editor.current_used_tool)
+		{
+			case MeshTools.SetHeight:
+				foreach (SelectedVertices sv in selected_vertices) 
+				{
+					main.mesh_editor.SetHeight (sv, main.mesh_editor.s_height);
+				}
+				break;
+			case MeshTools.Raise:
+				foreach (SelectedVertices sv in selected_vertices) 
+				{
+					main.mesh_editor.Raise (sv, main.mesh_editor.raise_force);
+				}
+				break;
+			case MeshTools.FlatRaise:
+				Extremes extremes = main.mesh_editor.fr_force >= 0 ? Extremes.Minimum : Extremes.Maximum;
+				main.mesh_editor.FlatRaise (selected_vertices, main.mesh_editor.fr_force , extremes, 0.1f);
+				break;
+			case MeshTools.Flat:
+				if (main.mesh_editor.flat_options == FlatOptions.ToHitPoint) 
+				{
+					foreach (SelectedVertices sv in selected_vertices) 
+					{
+						main.mesh_editor.Flat (sv, new Vector3 (hit_point.x, start_hit_point.y, hit_point.z), main.mesh_editor.flat_force);
+					}
+				} 
+				else if(main.mesh_editor.flat_options == FlatOptions.ToMedianPoint) 
+				{
+					float average = main.utils.Average (selected_vertices);
+					foreach (SelectedVertices sv in selected_vertices) 
+					{
+						main.mesh_editor.FlatToAverage (sv, average, main.mesh_editor.flat_force);
+					}
+				}
+				break;
+			case MeshTools.Randomize:
+				float random = main.mesh_editor.raise_force;
+				Vector3 random_vector = new Vector3(Random.Range(-random,random),0, Random.Range(-random,random));
+				foreach (SelectedVertices sv in selected_vertices) 
+				{
+					main.mesh_editor.Randomize (sv, random_vector);
+				}
+			 	break;
+		}
+		foreach (SelectedVertices sv in selected_vertices) 
+		{
+			main.utils.UpdateMesh(sv);
+		}
+	}
+
+	void PainterAction(ref LPWE main, ref List<SelectedVertices> selected_vertices, Vector3 hit_point)
+	{
+		switch(main.world_painter.tools)
+		{
+			case PaintTools.Paint:
+				foreach (SelectedVertices sv in selected_vertices) 
+				{
+					main.world_painter.Paint(sv, main.world_painter.temp_color);
+				}
+				break;
+		}
+	}
+
+
+
+	void SelectPage(LPWE main)
+	{
+		switch(page_index)
+		{
+			case 0:
+				LPWE_Pages.TerrainCreatorPage(ref main, sensitive_buttons);
+				break;
+			case 1:
+				LPWE_Pages.MeshEditorPage(ref main);
+				break;
+			case 2:
+				LPWE_Pages.WorldPainterPage(ref main);
+				break;
+
+		}
+	}
+
+
+
+	void SetComponents(LPWE main)
+	{
+		main.mesh_editor = lpwe_components.GetComponent<LPWE_MeshEditor>();
+		main.world_painter = lpwe_components.GetComponent<LPWE_WorldPainter>();
+		main.terrain_creator = lpwe_components.GetComponent<LPWE_TerrainCreator>();
+		main.utils = lpwe_components.GetComponent<LPWE_Utils>();
+		main.highlights = lpwe_components.GetComponent<LPWE_Highlights>();
+		main.selectors = lpwe_components.GetComponent<LPWE_Selectors>();
+
+		main.mesh_editor.SetLPWE(main);
+		main.world_painter.SetLPWE(main);
+		main.terrain_creator.SetLPWE(main);
+		main.utils.SetLPWE(main);
+		main.highlights.SetLPWE(main);
+		main.selectors.SetLPWE(main);
+	}
+
+
+	void Awake() 
+	{
+		LPWE main = (LPWE)target;
+		if(lpwe_components == null)
+		{
+			FindProps(main);
+		}		
+		SetComponents(main);
+		if (main.chunks == null) 
+		{
+			main.terrain_creator.LoadChunkMap ();
+		}
+		SetComponents(main);
+	}
+
+
+	void FindProps(LPWE main)
+	{
+		Transform[] childs = main.gameObject.GetComponentsInChildren<Transform>();
+		if(childs.Length <= 1)
+		{
+			lpwe_components = new GameObject();
+			lpwe_components.transform.parent = main.gameObject.transform;	
+			lpwe_components.name = "lpwe_props";
+		}
+		else
+		{
+			foreach(Transform t in childs)
+			{
+				if(t.gameObject.name == "lpwe_props")
+				{
+					lpwe_components = t.gameObject;
+				}
+			}
+		}
+	}
+}
---
+++ vikings and shit/Assets/Scripts/LPWE/LPWE_Selectors.cs
@@ -0,0 +1,285 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LPWE_Selectors : LPWE_Component 
+{
+	[SerializeField] public float radius;
+
+	public SelectionShape shape;
+
+	public Vector3 ColliderRaycastDetection() 
+	{
+		Ray ray = HandleUtility.GUIPointToWorldRay (Event.current.mousePosition);
+		RaycastHit hit;
+ 
+		if (Physics.Raycast (ray, out hit, 1000, 1 << LayerMask.NameToLayer("Terrain"))) 
+		{
+			return hit.point;
+		}
+		return Vector3.zero;
+	}
+
+	public Vector3 AxisRaycastDetection() 
+	{
+		Ray ray = HandleUtility.GUIPointToWorldRay (Event.current.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit, 1000, 1 << LayerMask.NameToLayer("Water"))) 
+		{
+			return hit.point;
+		}
+		return Vector3.zero;
+	}
+
+	public List<SelectedVertices> FindInRange(Vector3 point, float radius, HitPoint scan_type, bool vp) 
+	{
+		
+		List<int> nearest_chunks = NearestChunks(point);
+
+		List<SelectedVertices> selected_vertices = new List<SelectedVertices> ();
+		if(shape == SelectionShape.Circle)
+		{
+			foreach (int i in nearest_chunks) 
+			{
+
+				if(vp)
+				{
+					selected_vertices.Add(FindFaces3D(main.chunks [i], point, radius));
+					continue;
+				}
+
+				switch(scan_type) 
+				{
+					case HitPoint.SeaLevel2D:
+						selected_vertices.Add (RoundScan2D (main.chunks [i], point, radius));
+						break;
+					case HitPoint.HitPoint3D:
+						selected_vertices.Add (RoundScan3D (main.chunks [i], point, radius));
+						break;
+					case HitPoint.HitPoint2D:
+						selected_vertices.Add (RoundScan2D (main.chunks [i], point, radius));
+						break;
+				}
+			}
+		}
+		else if(shape == SelectionShape.Rectangle)
+		{
+			foreach (int i in nearest_chunks) 
+			{
+				if(vp)
+				{
+					selected_vertices.Add(FindFaces3D(main.chunks [i], point, radius));
+					continue;
+				}
+
+
+				switch(scan_type) 
+				{
+					case HitPoint.SeaLevel2D:
+						selected_vertices.Add (SquareScan2D (main.chunks [i], point, radius));
+						break;
+					case HitPoint.HitPoint3D:
+						selected_vertices.Add (SquareScan3D (main.chunks [i], point, radius));
+						break;
+					case HitPoint.HitPoint2D:
+						selected_vertices.Add (SquareScan2D (main.chunks [i], point, radius));
+						break;
+				}
+			}
+		}
+		return selected_vertices;
+	}
+
+
+	SelectedVertices RoundScan2D (Chunk chunk, Vector3 point, float size) 
+	{
+
+		List<SV> vertices = new List<SV> ();
+		MeshFilter mf = chunk.plane.GetComponent<MeshFilter> ();
+
+		for(int i = 0; i < mf.sharedMesh.vertices.Length; i++)
+		{
+
+			Vector3 real_pos = mf.sharedMesh.vertices [i] + chunk.plane.transform.position;
+
+			float vx = real_pos.x;
+			float vz = real_pos.z;
+
+			float dst = Vector2.Distance (new Vector2(point.x, point.z), new Vector2(vx, vz)); 
+
+			if (dst < size) 
+			{
+				vertices.Add (new SV(i));
+			}
+		}
+		return new SelectedVertices(chunk.chunk_serial_index, vertices);
+	}
+
+
+	SelectedVertices SquareScan2D (Chunk chunk, Vector3 point, float size) 
+	{
+
+		List<int> vertices = new List<int> ();
+		MeshFilter mf = chunk.plane.GetComponent<MeshFilter> ();
+
+		for(int i = 0; i < mf.sharedMesh.vertices.Length; i++)
+		{
+
+			Vector3 real_pos = mf.sharedMesh.vertices [i] + chunk.plane.transform.position;
+
+			float vx = real_pos.x;
+			float vz = real_pos.z;
+
+			bool bounds_x = real_pos.x > point.x - size && real_pos.x < point.x + size;
+			bool bounds_z = real_pos.z > point.z - size && real_pos.z < point.z + size;
+
+			if (bounds_x && bounds_z) 
+			{
+				vertices.Add (i);
+			}
+		}
+		return new SelectedVertices(chunk.chunk_serial_index, vertices);
+	}
+
+
+	SelectedVertices RoundScan3D (Chunk chunk, Vector3 point, float size) 
+	{
+
+		List<int> vertices = new List<int> ();
+		MeshFilter mf = chunk.plane.GetComponent<MeshFilter> ();
+
+		for(int i = 0; i < mf.sharedMesh.vertices.Length; i++) 
+		{
+			Vector3 real_pos = mf.sharedMesh.vertices [i] + chunk.plane.transform.position;
+
+			float dst = Vector3.Distance (point, real_pos); 
+
+			if (dst < size) 
+			{
+				vertices.Add (i);
+			}
+		}
+		return new SelectedVertices(chunk.chunk_serial_index, vertices);
+	}
+
+
+	SelectedVertices SquareScan3D (Chunk chunk, Vector3 point, float size) 
+	{
+
+		List<SV> vertices = new List<SV> ();
+		MeshFilter mf = chunk.plane.GetComponent<MeshFilter> ();
+
+		for(int i = 0; i < mf.sharedMesh.vertices.Length; i++) 
+		{
+			Vector3 real_pos = mf.sharedMesh.vertices [i] + chunk.plane.transform.position;
+
+			bool bounds_x = real_pos.x > point.x - size && real_pos.x < point.x + size;
+			bool bounds_y = real_pos.y > point.y - size && real_pos.y < point.y + size; 
+			bool bounds_z = real_pos.z > point.z - size && real_pos.z < point.z + size; 
+
+			if (bounds_x && bounds_y && bounds_z)
+			{
+				vertices.Add (new SV(i));
+			}
+		}
+		return new SelectedVertices(chunk.chunk_serial_index, vertices);
+	}
+
+	SelectedVertices FindFaces3D (Chunk chunk, Vector3 point, float size) 
+	{
+
+		List<int> vertices = new List<int> ();
+		MeshFilter mf = chunk.visual.GetComponent<MeshFilter> ();
+
+		for(int i = 0; i < mf.sharedMesh.vertices.Length; i+=3) 
+		{
+			Vector3 real_pos = mf.sharedMesh.vertices [i] + chunk.plane.transform.position;
+
+			float dst = Vector3.Distance (point, real_pos); 
+
+			if (dst < size) 
+			{
+				vertices.Add (i);
+			}
+		}
+		return new SelectedVertices(chunk.chunk_serial_index, vertices);
+	}
+
+
+	//unused outdated
+	public int FindNearest(Vector3 point) 
+	{
+		
+		Vector3[] vertices = main.terrain_mesh.vertices;
+		int i = 0;
+		float smallest_dst = Vector3.Distance(point, vertices[i]);
+		int index = -1;
+		i++;
+		for (; i < vertices.Length; i++) 
+		{
+			float dst = Vector3.Distance(point, vertices[i]);
+			if (dst < smallest_dst) 
+			{
+				smallest_dst = dst;
+				index = i;
+			}
+		}
+		return index;
+	}
+
+
+	//unused outdated
+	public int FindNearestHorizontal(Vector3 point) 
+	{
+		
+		Vector3[] vertices = main.terrain_mesh.vertices;
+		Vector2[] vertices_2D = new Vector2[vertices.Length];
+		int i = 0;
+		vertices_2D [i] = new Vector2 (vertices[i].x, vertices[i].z);
+		float smallest_dst = Vector2.Distance(new Vector2(point.x, point.z), vertices_2D[i]);
+		int index = -1;
+		i++;
+
+		for (; i < vertices_2D.Length; i++) 
+		{
+			vertices_2D [i] = new Vector2 (vertices[i].x, vertices[i].z);
+			float dst =  Vector2.Distance(new Vector2(point.x, point.z), vertices_2D[i]);
+			if (dst < smallest_dst) 
+			{
+				smallest_dst = dst;
+				index = i;
+			}
+		}
+		return index;
+	}		
+
+
+	List<int> NearestChunks(Vector3 point) 
+	{	
+		List<int> selected_chunks = new List<int> ();
+		int x_index = (int)point.x / 10;
+		int z_index = (int)point.z / 10;
+
+		int width = main.terrain_creator.width;
+		int lenght = main.terrain_creator.lenght;
+
+		for (int z = z_index - 1; z <= z_index + 1; z++) 
+		{
+			for (int x = x_index - 1; x <= x_index + 1; x++) 
+			{
+				if (x < 0 || z < 0 || z >= main.terrain_creator.width || x >= lenght) 
+				{
+					continue;
+				} 
+				else 
+				{
+					selected_chunks.Add (main.chunks [z * lenght + x].chunk_serial_index);
+				}
+			}
+		}
+
+		return selected_chunks;
+	}
+}
